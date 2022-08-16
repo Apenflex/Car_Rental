da38e646b881f7b4ac8d4477435882873d301d02
Sviatoslav <blackdbf@gmail.com>
2022-08-16 13:05:45 +0300

    add executive block

---
+++ src/js/swiper_open.js
@@ -0,0 +1,17 @@
+const swiperOpen = document.querySelectorAll(".offices_image");
+const swiperOverlay = document.querySelector(".overlay_swiper_block");
+const swiperCloseButton = document.querySelector(".swiper__close");
+
+
+swiperOpen.forEach(function (item) {
+    item.addEventListener("click", function (e) {
+        swiperOverlay.classList.add("overlay_swiper_active");
+    });
+});
+
+
+swiperCloseButton.addEventListener("click", function (e) {
+        if (swiperOverlay.classList.contains("overlay_swiper_active")) {
+            swiperOverlay.classList.remove("overlay_swiper_active");
+        }
+});
