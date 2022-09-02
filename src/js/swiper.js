de72f182950df6f8fa8f266478efc2a40538b6dd
Sviatoslav <blackdbf@gmail.com>
2022-09-02 15:38:33 +0300

    add slider for feedback section

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -20,5 +20,31 @@ const swiper = new Swiper('.swiper', {
     //   el: '.swiper-scrollbar',
     // },
 });
+const swiperFeedback = new Swiper('.swiperFeedback', {
+  slidesPerView: 1,
+  // Optional parameters
+  direction: 'horizontal',
+  
+  // loop: true,
+  // loopedSlides : 3,
+  
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
 
 
