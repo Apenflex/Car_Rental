571e0b88b7bd62a5001eed1933baf2f96d3ce97e
Sviatoslav <blackdbf@gmail.com>
2022-09-03 16:18:36 +0300

    add section faq

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -20,6 +20,7 @@ const swiper = new Swiper('.swiper', {
     //   el: '.swiper-scrollbar',
     // },
 });
+
 const swiperFeedback = new Swiper('.swiperFeedback', {
   slidesPerView: 1,
   // Optional parameters
@@ -47,4 +48,44 @@ const swiperFeedback = new Swiper('.swiperFeedback', {
   // },
 });
 
+const swiperBrands = new Swiper('.swiperBrands', {
+  loop: true,
+  // direction: 'horizontal',
+  // slidesPerView: 1,
+  
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      
+    },
+    // when window width is >= 640px
+    992: {
+      slidesPerView: 3,
+      
+    }
+  },
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
