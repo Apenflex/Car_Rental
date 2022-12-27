d8dbed6a9bea43c18181fa5cea47fa4062351222
Sviatoslav <blackdbf@gmail.com>
2022-12-27 13:11:43 +0200

    added gsap animations

--- dist/js/swiper.js
+++ dist/js/swiper.js
@@ -1,93 +1,112 @@
-const swiper = new Swiper('.swiper', {
+const swiperOpen = document.querySelectorAll(".offices_image");
+const swiperOverlay = document.querySelector(".overlay_swiper_block");
+const swiperCloseButton = document.querySelector(".swiper__close");
+
+swiperOpen.forEach(function (item) {
+    item.addEventListener("click", function (e) {
+        swiperOverlay.classList.add("overlay_swiper_active");
+    });
+});
+
+// swiperOverlay.addEventListener("click", (e) => {
+//     if (e.target === swiperOverlay) {
+//         swiperOverlay.classList.remove("overlay_swiper_active");
+//     }
+// });
+
+swiperCloseButton.addEventListener("click", function (e) {
+    if (swiperOverlay.classList.contains("overlay_swiper_active")) {
+        swiperOverlay.classList.remove("overlay_swiper_active");
+    }
+});
+
+const swiper = new Swiper(".swiper", {
     // Optional parameters
     // direction: 'vertical',
     // loop: true,
-    
-  
+
     // If we need pagination
     // pagination: {
     //   el: '.swiper-pagination',
     // },
-  
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-  
+
     // And if we need scrollbar
     // scrollbar: {
     //   el: '.swiper-scrollbar',
     // },
 });
 
-const swiperFeedback = new Swiper('.swiperFeedback', {
-  slidesPerView: 1,
-  // Optional parameters
-  direction: 'horizontal',
-  
-  // loop: true,
-  // loopedSlides : 3,
-  
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+const swiperFeedback = new Swiper(".swiperFeedback", {
+    slidesPerView: 1,
+    // Optional parameters
+    direction: "horizontal",
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    // loop: true,
+    // loopedSlides : 3,
 
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
 });
 
-const swiperBrands = new Swiper('.swiperBrands', {
-  loop: true,
-  // direction: 'horizontal',
-  // slidesPerView: 1,
-  
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-    },
-    // when window width is >= 576px
-    576: {
-      slidesPerView: 2,
-    },
-    // when window width is >= 768px
-    768: {
-      slidesPerView: 2,
-      
-    },
-    // when window width is >= 992px
-    992: {
-      slidesPerView: 3,
-      
-    }
-  },
-  
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+const swiperBrands = new Swiper(".swiperBrands", {
+    loop: true,
+    // direction: 'horizontal',
+    // slidesPerView: 1,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+        },
+        // when window width is >= 576px
+        576: {
+            slidesPerView: 2,
+        },
+        // when window width is >= 768px
+        768: {
+            slidesPerView: 2,
+        },
+        // when window width is >= 992px
+        992: {
+            slidesPerView: 3,
+        },
+    },
 
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
 });
--- src/js/gsap_animate.js
+++ src/js/gsap_animate.js
@@ -82,30 +82,194 @@ gsap.from(".about_us-btn", {
 ScrollTrigger.create({
     trigger: ".history_card-black",
     start: "top top",
-    markers: true,
+    // markers: true,
     end: "+=300",
     pin: true,
 });
 ScrollTrigger.create({
     trigger: ".history_card-white",
     start: "top top",
-    markers: true,
+    // markers: true,
     end: "+=300",
     pin: true,
 });
 ScrollTrigger.create({
     trigger: ".history_card-black-light",
     start: "top top",
-    markers: true,
+    // markers: true,
     end: "+=300",
     pin: true,
 });
 ScrollTrigger.create({
     trigger: ".history_card-white-dark",
     start: "top top",
-    markers: true,
+    // markers: true,
     end: "+=100",
     pin: true,
 });
 
-
+// Scroll animation responsibility
+gsap.from(".section_name-responsibility", {
+    scrollTrigger: {
+        trigger: ".responsibility",
+        start: "top 39%",
+        // markers: true,
+        end: "+=100px",
+    },
+    x: -100,
+    opacity: 0,
+});
+gsap.from(".title__responsibility", {
+    scrollTrigger: {
+        trigger: ".responsibility",
+        start: "top 40%",
+        // markers: true,
+        end: "+=100px",
+    },
+    x: -100,
+    opacity: 0,
+    delay: 0.2,
+});
+gsap.from(".subtitle__responsibility", {
+    scrollTrigger: {
+        trigger: ".responsibility",
+        start: "top 10%",
+        // markers: true,
+        end: "+=100px",
+    },
+    y: 100,
+    opacity: 0,
+    delay: 0.3,
+});
+gsap.from(".responsibility-img", {
+    scrollTrigger: {
+        trigger: ".responsibility",
+        start: "10% center",
+        // markers: true,
+        end: "+=650px",
+        scrub: true,
+    },
+    y: 500,
+    transformOrigin: "top top",
+    ease: "none",
+});
+gsap.from(".responsibility-item", {
+    scrollTrigger: {
+        trigger: ".responsibility",
+        start: "20% 20%",
+        // markers: true,
+        end: "+=200px",
+    },
+    x: -100,
+    opacity: 0,
+    delay: 0.3,
+    stagger: 0.2,
+});
+gsap.from(".responsibility-btn", {
+    scrollTrigger: {
+        trigger: ".descr__responsibility",
+        start: "top 40%",
+        // markers: true,
+        end: "+=50px",
+    },
+    y: 50,
+    opacity: 0,
+    delay: 0.2,
+});
+// Scroll animation offer
+gsap.from(".card_items", {
+    scrollTrigger: {
+        trigger: ".offer",
+        start: "top top",
+        // markers: true,
+        end: "+=350px",
+        scrub: true,
+    },
+    y: 140,
+    transformOrigin: "top top",
+    ease: "none",
+});
+// Scroll animation offer
+gsap.from(".executive_contacts", {
+    scrollTrigger: {
+        trigger: ".wrapper__executive",
+        start: "top 20%",
+        // markers: true,
+        end: "+=50px",
+    },
+    x: -100,
+    opacity: 0,
+});
+gsap.from(".executive_offers", {
+    scrollTrigger: {
+        trigger: ".wrapper__executive",
+        start: "top 20%",
+        // markers: true,
+        end: "+=50px",
+    },
+    y: 50,
+    opacity: 0,
+    delay: 0.3,
+});
+// Scroll animation rates
+gsap.from(".rates_card-bg", {
+    scrollTrigger: {
+        trigger: ".rates",
+        start: "5% 20%",
+        // markers: true,
+        end: "+=400px",
+        scrub: true,
+    },
+    y: 350,
+    opacity: 0,
+    transformOrigin: "bottom bottom",
+    ease: "none",
+});
+gsap.from(".rates_card-left", {
+    scrollTrigger: {
+        trigger: ".rates",
+        start: "5% 30%",
+        // markers: true,
+        end: "+=200px",
+        scrub: true,
+    },
+    x: 200,
+    transformOrigin: "left left",
+    ease: "none",
+});
+gsap.from(".rates_card-right", {
+    scrollTrigger: {
+        trigger: ".rates",
+        start: "5% 20%",
+        // markers: true,
+        end: "+=200px",
+        scrub: true,
+    },
+    x: -200,
+    transformOrigin: "right right",
+    ease: "none",
+});
+// Scroll animation staff
+gsap.from(".card-title", {
+    scrollTrigger: {
+        trigger: ".staff_wrapper",
+        start: "30% 40%",
+        // markers: true,
+        end: "+=40px",
+    },
+    y: -70,
+    opacity: 0,
+    delay: 0.2,
+});
+gsap.from(".staff-divider", {
+    scrollTrigger: {
+        trigger: ".staff_wrapper",
+        start: "30% 40%",
+        // markers: true,
+        end: "+=40px",
+    },
+    scale: 0,
+    opacity: 0,
+    delay: 0.2,
+    duration: 0.9,
+});
